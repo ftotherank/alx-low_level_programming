0c866ac79136e0a5239b792b05a1fea920e5a771
ftotherank <franknashe1738@gmail.com>
2023-02-16 02:39:10 -0800

    correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ *main - Entry point
  *
- * Description: A c program that print with put function
+ *Description: A c program that print with put function
  *
- * Return: Always 0 (Success)
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
-    puts("\"programming is like building a multilingual puzzle");
+    puts("\"Programming is like building a multilingual puzzle");
     return (0);
 }    
